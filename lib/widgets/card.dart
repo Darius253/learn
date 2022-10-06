2b8ed1cf3823d9d294f74acc7fef75e7e124b28e
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-06 01:06:11 +0000

    Added new features

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:learn/shared/exports.dart';
 
-class SchoolCard extends StatefulWidget {
+class SchoolCard extends StatelessWidget {
   final String image;
   final String school;
   final String info;
@@ -14,50 +13,50 @@ class SchoolCard extends StatefulWidget {
       required this.onTap})
       : super(key: key);
 
-  @override
-  State<SchoolCard> createState() => _SchoolCardState();
-}
 
-class _SchoolCardState extends State<SchoolCard> {
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
     double width = MediaQuery.of(context).size.width;
     return InkWell(
-      onTap: widget.onTap,
+      onTap: onTap,
       child: Container(
         color: Colors.transparent,
         width: width * 0.8,
         height: height * 0.5,
         child: Stack(children: [
-          Column(children: [
-            Expanded(
-                flex: 3,
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(16),
-                  child: Image.asset(
-                    widget.image,
-                    fit: BoxFit.cover,
-                  ),
-                )),
-            Expanded(
-                flex: 1,
-                child: Container(
-                  width: width * 0.8,
-                  padding: const EdgeInsets.all(10.0),
-                  color: const Color.fromARGB(253, 32, 91, 146),
-                  child: Text(
-                    widget.info,
-                    style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      height: 2,
-                      fontFamily: 'Poppins',
-                      fontWeight: FontWeight.w500,
+          Container(
+            color: const Color.fromARGB(248, 32, 91, 146),
+            child: Column(children: [
+              Expanded(
+                  flex: 3,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(width * 0.02),
+                    child: Image.asset(
+                      image,
+                      fit: BoxFit.cover,
+                      width: width * 5,
                     ),
-                  ),
-                ))
-          ]),
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    width: width * 0.8,
+                    padding: const EdgeInsets.all(10.0),
+                    color: const Color.fromARGB(253, 32, 91, 146),
+                    child: Text(
+                      info,
+                      style: const TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        height: 2,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ))
+            ]),
+          ),
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: ClipRRect(
@@ -67,7 +66,7 @@ class _SchoolCardState extends State<SchoolCard> {
                 color: Colors.white,
                 width: 150,
                 height: 24,
-                child: Text(widget.school,
+                child: Text(school,
                     style: const TextStyle(
                         fontFamily: 'Poppins',
                         fontWeight: FontWeight.w500,
