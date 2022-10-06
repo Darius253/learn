2c8a0bbec5a201e1c8d3c08b5bafbe74daa01c83
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-06 01:05:45 +0000

    Added new features

--- lib/mainScreens/home.dart
+++ lib/mainScreens/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:learn/widgets/card.dart';
+import 'package:learn/shared/exports.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,27 +13,46 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      scrollDirection: Axis.horizontal,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            Container(
-              color: Colors.white,
-              width: 300,
-              height: 150,
-            ),
-            const SizedBox(
-              height: 50,
-            ),
-            Row(children: [
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                width: 300,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          CupertinoIcons.search,
+                          color:  Color.fromARGB(248, 32, 91, 146),
+                        ),
+                        suffix: Icon(
+                          CupertinoIcons.trash,
+                          color: Color.fromARGB(248, 32, 91, 146),
+                        )))),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+          SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
+            child: Row(children: [
               SchoolCard(
                 school: 'Junior High',
                 info: 'Tap Here to view all  Junior High School Subjects ',
                 image: 'assets/images/junior.jpg',
-                onTap: () {},
+                onTap: () => Get.to(
+                  () => const JHSCourses(),
+                ),
               ),
               const SizedBox(
                 width: 20,
@@ -44,8 +64,8 @@ class _HomeState extends State<Home> {
                 onTap: () => ((() {})),
               )
             ]),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
