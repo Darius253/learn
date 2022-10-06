f36c13049181b6948a3d962b458f8f75e1971211
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-06 01:07:36 +0000

    Added New File ✨🆕

---
+++ lib/widgets/subject_cards.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class SubjectCard extends StatelessWidget {
+  final String image;
+  final String name;
+  final VoidCallback onTap;
+  const SubjectCard(
+      {Key? key, required this.name, required this.image, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+          color: const Color.fromARGB(248, 32, 91, 146),
+          elevation: 2.0,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(7),
+                  child: Container(
+                    color: Colors.grey,
+                    child: Center(child: Text(image)),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: height * 0.045,
+                  color: const Color.fromARGB(248, 32, 91, 146),
+                  child: Center(
+                      child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontSize: 15,
+                      color: Colors.white,
+                      height: 2,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  )),
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
