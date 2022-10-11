8b4733c923a58300a2ed23eedb645709fcf9ca86
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-11 01:49:40 +0000

    ✨✔ Made modifications

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -20,8 +20,13 @@ export 'package:learn/mainScreens/subject_details.dart';
 export 'package:learn/widgets/subject_cards.dart';
 export 'package:learn/mainScreens/jhs_courses.dart';
 export 'package:learn/widgets/card.dart';
+export '../models/user_model.dart';
+export 'package:learn/models/subjects_model.dart';
+
 
 //pub.dev
+export 'package:firebase_storage/firebase_storage.dart';
+export 'package:firebase_auth/firebase_auth.dart';
 export 'package:delayed_display/delayed_display.dart';
 export 'package:smooth_page_indicator/smooth_page_indicator.dart';
 export 'package:flutter_svg/flutter_svg.dart';
