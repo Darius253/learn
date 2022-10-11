d01a71d273fb59b5e197d54e49a8122ed46def7c
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-11 01:44:53 +0000

    Added New file

---
+++ lib/controllers/subjects_controller.dart
@@ -0,0 +1,5 @@
+import 'package:learn/shared/exports.dart';
+
+class SubjectController extends GetxController {
+  var subject = <Subject>[].obs;
+}
---
+++ lib/controllers/user_controller.dart
@@ -0,0 +1,7 @@
+import 'package:learn/shared/exports.dart';
+
+
+
+class PersonController extends GetxController {
+  var person = <Person>[].obs;
+}
