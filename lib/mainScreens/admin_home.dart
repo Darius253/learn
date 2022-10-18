905cb68d4a63ff116f8047befb146348ebb97f8c
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-18 00:33:27 +0000

    🆕 Added Uploading Page for Admin

--- lib/mainScreens/admin_home.dart
+++ lib/mainScreens/admin_home.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learn/widgets/uploaded_subjects.dart';
+import 'package:learn/widgets/uploading_widget.dart';
 
 class AdminHome extends StatefulWidget {
   const AdminHome({super.key});
@@ -8,13 +11,32 @@ class AdminHome extends StatefulWidget {
 }
 
 class _AdminHomeState extends State<AdminHome> {
+  late bool _show = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-      ),
-      body: Container(),
-    );
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(CupertinoIcons.add),
+          onPressed: () {
+            // setState(() {
+            //   _show = !_show;
+
+            // });
+            addDataWidget(BuildContext, context);
+          },
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ListView.builder(
+              itemCount: 10,
+              itemBuilder: (BuildContext context, int index) {
+                return const UploadedSubject(
+                  subjectname: 'Mathematics',
+                );
+              },
+            ),
+          ),
+        ));
   }
 }
