5da99bccb3a1ca3cf9c897a5d59c75df9cad08d9
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-23 00:56:30 +0000

    🆕Added new file

---
+++ lib/mainScreens/shs_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:learn/mainScreens/shs_courses.dart';
+
+import '../shared/exports.dart';
+
+class ShsPage extends StatefulWidget {
+  const ShsPage({super.key});
+
+  @override
+  State<ShsPage> createState() => _ShsPageState();
+}
+
+class _ShsPageState extends State<ShsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            elevation: 0.0,
+            backgroundColor: Colors.white,
+            title: const Text(
+              'Senior High',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w400),
+            ),
+            leading: IconButton(
+              icon: const Icon(CupertinoIcons.back, color: Colors.black),
+              onPressed: () => Get.back(),
+            )),
+        body: Container(
+            padding: const EdgeInsets.all(12.0),
+            child: GridView(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 2.0,
+                  mainAxisSpacing: 2.0),
+              children: [
+                SubjectCard(
+                  name: 'SHS 1',
+                  image: 'Darius',
+                  onTap: () => Get.to(() => const SHSCourses(
+                        formname: 'SHS 1',
+                      )),
+                ),
+                SubjectCard(
+                  name: 'SHS 2',
+                  image: 'Darius',
+                  onTap: () => Get.to(() => const SHSCourses(
+                        formname: 'SHS 2',
+                      )),
+                ),
+                SubjectCard(
+                  name: 'SHS 3',
+                  image: 'Darius',
+                  onTap: () => Get.to(() => const SHSCourses(
+                        formname: 'SHS 3',
+                      )),
+                ),
+              ],
+            )));
+  }
+}
