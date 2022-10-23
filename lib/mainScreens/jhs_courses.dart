6623220d3ddbf792be1d5b00e8859039314bba02
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-23 00:55:28 +0000

    ⚙Made few changes

--- lib/mainScreens/jhs_courses.dart
+++ lib/mainScreens/jhs_courses.dart
@@ -1,9 +1,15 @@
+import 'dart:collection';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:learn/controllers/subjects_controller.dart';
+import 'package:learn/services/database.dart';
 import '../shared/exports.dart';
 
 class JHSCourses extends StatefulWidget {
-  const JHSCourses({Key? key}) : super(key: key);
+  final String formname;
+  const JHSCourses({Key? key, required this.formname}) : super(key: key);
 
   @override
   State<JHSCourses> createState() => _JHSCoursesState();
@@ -11,37 +17,74 @@ class JHSCourses extends StatefulWidget {
 
 class _JHSCoursesState extends State<JHSCourses> {
   @override
+  void initState() {
+    SubjectController subject = Get.put(SubjectController());
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            elevation: 0.0,
-            backgroundColor: Colors.white,
-            title: const Text(
-              'JHS Subjects',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w400),
-            ),
-            leading: IconButton(
-              icon: const Icon(CupertinoIcons.back, color: Colors.black),
-              onPressed: () => Get.back(),
-            )),
-        body: Container(
-            padding: const EdgeInsets.all(12.0),
-            child: GridView.builder(
-              itemCount: 8,
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 2.0,
-                  mainAxisSpacing: 2.0),
-              itemBuilder: (BuildContext context, int index) {
-                return SubjectCard(
-                  name: 'Mathematics',
-                  image: 'Darius',
-                  onTap: () => Get.to(() => const SubjectDetails()),
-                );
-              },
-            )));
+      appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          title: const Text(
+            'JHS Subjects',
+            style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w400),
+          ),
+          leading: IconButton(
+            icon: const Icon(CupertinoIcons.back, color: Colors.black),
+            onPressed: () => Get.back(),
+          )),
+      body: FutureBuilder(
+        future: FirestoreService.subjectsCollection
+            .where('form', isEqualTo: widget.formname)
+            .get(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator.adaptive(),
+            );
+          }
+
+          return GridView(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, crossAxisSpacing: 2.0, mainAxisSpacing: 2.0),
+            children: snapshot.data!.docs.map((document) {
+              return SubjectCard(
+                  name: document['name'],
+                  image: '',
+                  onTap: ((() => Get.to(() => SubjectDetails(
+                        formname: '${widget.formname}  ' '${document['name']}',
+                      )))));
+            }).toList(),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<List<Subject>> getsubjects() async {
+    CollectionReference ref = FirestoreService.subjectsCollection;
+    QuerySnapshot eventsQuery =
+        await ref.where('form', isEqualTo: widget.formname).get();
+
+    HashMap<String, Subject> eventsHashMap = HashMap<String, Subject>();
+
+    for (var document in eventsQuery.docs) {
+      eventsHashMap.putIfAbsent(
+          document['id'],
+          () => Subject(
+              name: document['name'],
+              form: document['form'],
+              school: document['school'],
+              file: document['file'],
+              about: document['about']));
+    }
+
+    return eventsHashMap.values.toList();
   }
 }
