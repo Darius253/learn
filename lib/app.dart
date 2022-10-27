6a826d2622c4a52ac44db5b8d0ff3ceaaa67ddcb
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-27 01:35:22 +0000

    🆕🔨Added Fileviewer

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_file_view/flutter_file_view.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:learn/shared/exports.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +27,10 @@ class App extends StatelessWidget {
       title: 'Learn',
       color: Color.fromARGB(255, 67, 64, 255),
       debugShowCheckedModeBanner: false,
-      localizationsDelegates: <LocalizationsDelegate>[
+      localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
         DefaultMaterialLocalizations.delegate,
         DefaultWidgetsLocalizations.delegate,
       ],
