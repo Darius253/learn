cdafafbca1c66606e7c4adf5793e16fafee3c63f
Darius <68332672+Darius253@users.noreply.github.com>
2022-10-29 02:09:18 +0000

    🖼Added Images

---
+++ lib/widgets/images.dart
@@ -0,0 +1,25 @@
+class Images {
+  static const String imagesPath = 'assets/images/';
+  static const String junior =
+      'https://www.achieve3000.com/media/images/AL_bannerMobile.original.png';
+  static const String junior2 =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6asnFyBy3YskEpnoZvlsyx0ozaITlZjAslA&usqp=CAU';
+  static const String junior3 =
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsqgGaNUBPelgyvxH61ZkAq935KNBRMBSSKQ&usqp=CAU';
+  static const String senior =
+      'https://www.openaccessgovernment.org/wp-content/uploads/2018/11/dreamstime_s_47450085.jpg';
+  static const String senior2 =
+      'https://www.incimages.com/uploaded_files/image/1920x1080/getty_621371400_276916.jpg';
+  static const String senior3 =
+      'https://i0.wp.com/elt-connect.com/wp-content/uploads/2016/05/goalsetting_198458453.jpg?fit=1000%2C667&ssl=1';
+  static const String image =
+      'https://previews.123rf.com/images/rawpixel/rawpixel1603/rawpixel160308149/53554297-learn-learning-education-knowledge-wisdom-studying-concept.jpg';
+  static const String english =
+      'https://fluencycorp.com/wp-content/uploads/2019/01/hardest-part-learning-english.jpg';
+  static const String maths =
+      'https://www.careersportal.co.za/sites/default/files/styles/amp_1200x675_16_9/public/images/Terrique%20Faro/Maths_1.jpg?itok=tO4eBhoE';
+  static const String socialStudies =
+      'https://highschool-gh.s3.amazonaws.com/0042382edbe5a1cc95c8b32e9eb965a3_social%20studies.jpg';
+  static const String science =
+      'https://cdn2.vectorstock.com/i/1000x1000/46/21/chemistry-chemical-science-or-pharmacy-vector-22824621.jpg';
+}
