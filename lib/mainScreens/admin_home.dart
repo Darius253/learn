a244954e9b1711dc3951d1d6ed1ace62546f3590
Darius <68332672+Darius253@users.noreply.github.com>
2022-11-13 22:18:24 +0000

    🔨⚙ Made some modifications

--- lib/mainScreens/admin_home.dart
+++ lib/mainScreens/admin_home.dart
@@ -1,22 +1,22 @@
-import 'package:dotted_border/dotted_border.dart';
-import 'package:file_picker/file_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:learn/widgets/uploaded_subjects.dart';
 import 'package:learn/widgets/uploading_widget.dart';
 
+import '../services/database.dart';
 import '../shared/exports.dart';
-import '../widgets/upload_textfield.dart';
 
 class AdminHome extends StatefulWidget {
-  const AdminHome({super.key});
+  final String? id;
+  const AdminHome({super.key, this.id});
 
   @override
   State<AdminHome> createState() => _AdminHomeState();
 }
 
 class _AdminHomeState extends State<AdminHome> {
+  bool isdelete = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,18 +24,101 @@ class _AdminHomeState extends State<AdminHome> {
           onPressed: (() => Get.off(() => const UploadingWidget())),
           child: const Icon(CupertinoIcons.add),
         ),
+        appBar: AppBar(
+          elevation: 0.0,
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          actions: [
+            TextButton.icon(
+                onPressed: (() => AuthService().signOut()),
+                icon: const Icon(
+                  Icons.logout_sharp,
+                  color: Colors.red,
+                ),
+                label: const Text('SignOut')),
+            const SizedBox(
+              width: 10,
+            )
+          ],
+        ),
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ListView.builder(
-              itemCount: 10,
-              itemBuilder: (BuildContext context, int index) {
-                return const UploadedSubject(
-                  subjectname: 'Mathematics',
-                );
-              },
-            ),
-          ),
-        ));
+            child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: StreamBuilder<QuerySnapshot>(
+                    stream: FirestoreService.subjectsCollection.snapshots(),
+                    builder: (context, snapshot) {
+                      return (snapshot.connectionState ==
+                              ConnectionState.waiting
+                          ? const Center(
+                              child: CircularProgressIndicator.adaptive(),
+                            )
+                          : ListView.builder(
+                              itemCount: snapshot.data!.docs.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                var subject = snapshot.data!.docs[index].data()
+                                    as Map<String, dynamic>;
+                                return InkWell(
+                                  onTap: (() {
+                                    _showMyDialog();
+                                    delete(snapshot, index);
+                                  }),
+                                  child: UploadedSubject(
+                                    subjectname: '${subject['form']} '
+                                        ' ${subject['name']}',
+                                  ),
+                                );
+                              },
+                            ));
+                    }))));
   }
+
+  Future delete(
+      AsyncSnapshot<QuerySnapshot<Object?>> snapshot, int index) async {
+    if (isdelete == true) {
+      await FirebaseFirestore.instance
+          .runTransaction((Transaction myTransaction) async {
+        myTransaction.delete(snapshot.data!.docs[index].reference);
+      });
+      Get.snackbar('Succesful', 'Content Deleted',
+          snackPosition: SnackPosition.BOTTOM);
+    } else {
+      return null;
+    }
+  }
+
+  Future<void> _showMyDialog() async => showDialog<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Delete Content'),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: const <Widget>[
+                  Text('Proceed to Delete Content? '),
+                ],
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text(
+                  'Yes ',
+                  style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+                ),
+                onPressed: () async {
+                  setState(() {
+                    isdelete = true;
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+              TextButton(
+                  child: const Text(
+                    'No ',
+                    style: TextStyle(fontWeight: FontWeight.w700, fontSize: 15),
+                  ),
+                  onPressed: () => Navigator.pop(context))
+            ],
+          );
+        },
+      );
 }
