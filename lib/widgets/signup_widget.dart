e25d4da8bfd686093a5891a82085fd50fff0ffc3
Darius <68332672+Darius253@users.noreply.github.com>
2022-11-13 22:21:33 +0000

    🔨⚙ Made some modifications

--- lib/widgets/signup_widget.dart
+++ lib/widgets/signup_widget.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:learn/controllers/user_controller.dart';
-import 'package:learn/services/authservice.dart';
 import 'package:learn/shared/exports.dart';
 
 class SignupWidget extends StatefulWidget {
