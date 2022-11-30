b04dac4adbf7cabe989f5104fa9c619dcd69cc4c
Darius <68332672+Darius253@users.noreply.github.com>
2022-11-30 14:08:06 +0000

    🔨✔✨Added flutter native splash screen and built final apk

--- lib/mainScreens/admin_home.dart
+++ lib/mainScreens/admin_home.dart
@@ -1,10 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:learn/widgets/uploaded_subjects.dart';
-import 'package:learn/widgets/uploading_widget.dart';
-
-import '../services/database.dart';
 import '../shared/exports.dart';
 
 class AdminHome extends StatefulWidget {
