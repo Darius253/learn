41909320dadc9a46eece9dcf8c482fc73bc396bf
Darius <68332672+Darius253@users.noreply.github.com>
2022-11-30 14:08:14 +0000

    🔨✔✨Added flutter native splash screen and built final apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:learn/shared/exports.dart';
-
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp().then((value) => Get.put(Wrapper()));
   runApp(const MyApp());
 }
+ 
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
