428f1b10883c2b6305813a04f22ba7f5c4b960a2
Darius <68332672+Darius253@users.noreply.github.com>
2022-11-30 14:07:41 +0000

    🔨✔✨Added flutter native splash screen and built final apk

--- lib/mainScreens/form_page.dart
+++ lib/mainScreens/form_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import '../shared/exports.dart';
-import '../widgets/images.dart';
 
 class FormPage extends StatefulWidget {
   const FormPage({super.key});
