e0b59b675eb5706ff65a9fc111e2fe4b68850fa2
Darius <68332672+Darius253@users.noreply.github.com>
2022-11-30 14:07:51 +0000

    🔨✔✨Added flutter native splash screen and built final apk

--- lib/mainScreens/discover.dart
+++ lib/mainScreens/discover.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../shared/exports.dart';
+
 class Discover extends StatefulWidget {
   const Discover({Key? key}) : super(key: key);
 
@@ -10,8 +12,65 @@ class Discover extends StatefulWidget {
 class _DiscoverState extends State<Discover> {
   @override
   Widget build(BuildContext context) {
-    return const  Center(
-      child: Text('Discover')
+    return Center(
+      child: Container(
+        color: Colors.white,
+        child: ListView(
+          children: [
+            InkWell(
+              onTap: () => _launchURL('https://www.adesuaonline.net/home'),
+              child: const DiscoverMore(
+                info: 'Discover more on your favorite Subjects',
+                url: ' Visit AdesuaOnline.net',
+              ),
+            ),
+            InkWell(
+              onTap: () => _launchURL('https://icampusgh.com/login/index.php'),
+              child: const DiscoverMore(
+                info: 'View Different Courses',
+                url: ' Checkout icampusgh',
+              ),
+            ),
+            InkWell(
+              onTap: () => _launchURL('https://www.udemy.com/courses/free/'),
+              child: const DiscoverMore(
+                info: 'Need free courses?',
+                url: 'Udemy offers free courses',
+              ),
+            ),
+            InkWell(
+              onTap: () => _launchURL('https://www.skillshare.com/en/'),
+              child: const DiscoverMore(
+                info: 'Learn new skills ',
+                url: 'Skillshare is here to help',
+              ),
+            ),
+            InkWell(
+              onTap: () => _launchURL(
+                  'https://www.blackboard.com/en-eu/teaching-learning/learning-management/blackboard-learn'),
+              child: const DiscoverMore(
+                info: 'Explore variety of courses',
+                url: 'Learn with Blackboard',
+              ),
+            ),
+            InkWell(
+              onTap: () => _launchURL('https://moodle.org/'),
+              child: const DiscoverMore(
+                info: 'Learn more, Be more',
+                url: 'Moodle.org/',
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  Future<void> _launchURL(String link) async {
+    if (await canLaunchUrl(Uri.parse(link))) {
+      await launchUrl(Uri.parse(link));
+    } else {
+      throw 'Could not launch $link';
+    }
   }
 }
